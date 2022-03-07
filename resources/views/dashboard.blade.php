f03c523f319cf34f96c23b6d1f7d660111fc6c6e
Fooxiie <dev.fooxiie@gmail.com>
2022-03-07 23:58:01 +0100

    Btn de resume de commande sur le dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,10 @@
     </x-slot>
 
     <div class="py-12">
+        <a href="{{route('order.resume')}}" class="px-4 py-2 font-semibold text-sm bg-white text-slate-700 flex items-center mx-auto
+                                    border border-slate-300 rounded-md shadow-sm mb-8 w-48 text-center">
+            Générer la commmande du jour
+        </a>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
